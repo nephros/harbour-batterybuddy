75cc83d5cca4681df78d24c8dee33b9f019d0cb0
nephros <sailfish@nephros.org>
2023-02-23 16:06:30 +0100

    [PineTab] Support USB+AC Charging: application

--- application/src/battery.cpp
+++ application/src/battery.cpp
@@ -72,8 +72,7 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
     filenames.clear();
     filenames << "/sys/class/power_supply/usb/present"
               << "/sys/class/power_supply/dollar_cove_charger/present"
-              << "/sys/class/power_supply/axp20x-usb/present"
-              << "/sys/class/power_supply/axp20x-ac/present";
+              << "/sys/class/power_supply/axp20x-usb/present";
 
     foreach(const QString& file, filenames) {
         if(!chargerConnectedFile && QFile::exists(file)) {
@@ -86,7 +85,8 @@ Battery::Battery(Settings* newSettings, Logger* newLogger, QObject* parent) : QO
 
     // Number: 0 or 1
     filenames.clear();
-    filenames << "/sys/class/power_supply/ac/present";
+    filenames << "/sys/class/power_supply/ac/present"
+              << "/sys/class/power_supply/axp813-ac/present";
 
     foreach(const QString& file, filenames) {
         if(!acConnectedFile && QFile::exists(file)) {
